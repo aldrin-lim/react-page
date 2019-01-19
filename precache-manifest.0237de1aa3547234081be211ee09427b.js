8fa446d391245b10f367ef0aa16dd0fff184f0af
Aldrin Lim <aldrinlim81@gmail.com>
2019-01-19 20:51:35 +0800

    Updates

---
+++ precache-manifest.0237de1aa3547234081be211ee09427b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43eb6a92e2f3418f7a49",
+    "url": "/react-page/static/js/runtime~main.43eb6a92.js"
+  },
+  {
+    "revision": "5c30160bf64b90ae5f36",
+    "url": "/react-page/static/js/main.5c30160b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-page/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5c30160bf64b90ae5f36",
+    "url": "/react-page/static/css/main.b36def60.chunk.css"
+  },
+  {
+    "revision": "113d28bed2d1f6f49052159659a7fce1",
+    "url": "/react-page/index.html"
+  }
+];
